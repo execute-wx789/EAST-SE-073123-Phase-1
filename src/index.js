bc92089dd0990f4e2a25bf609bcb9c1b57a205d7
Yurick <YJakse789@gmail.com>
2023-08-07 10:18:42 -0400

    Further Progress

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@
 const bookList = document.querySelector("#book-list");
 const toggleBookFormButton = document.querySelector("#toggle-form");
 const bookForm = document.querySelector("#book-form"); 
+const storeForm = document.querySelector("#store-form");
 const toggleStoreFormButton = document.querySelector("#toggle-store-form");
+const storeSelector = document.querySelector("#store-selector")
+const url = "http://127.0.0.1:3000"
 //✅ 3. store drop-down list
 //✅ 3. store-form const
 
@@ -12,11 +15,46 @@ const toggleStoreFormButton = document.querySelector("#toggle-store-form");
   //✅ 2a. create fetch request: pessimistic and optimistic approach
   //✅ 2b. use boilerplate from request_helpers.js to execute POST request
 	//✅ 2c. use dev tools to simulate a slow collection (network) and add a loading class to li 
-  
+bookForm.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  const book = {
+    title: e.target.title.value,
+    author: e.target.author.value,
+    price: e.target.price.value,
+    reviews: [],
+    inventory: e.target.inventory.value,
+    imageUrl: e.target.imageUrl.value
+  }
+  fetch(`${url}/books`, {
+    method: "POST",
+    headers: {"content-type": "application/json"},
+    body: JSON.stringify(book)
+  })
+  .then(res => res.json())
+  .then(data=>renderBook(data))
+  .catch(err => alert("Something Went Wrong"))
+})
+
 
 //✅ 3. implement store form and store dropdown
 //✅ 3a. create eventListener for form
 	//✅ 3b. create POST request for new store
+storeForm.addEventListener("submit", (e)=>{
+  e.preventDefault()
+    const store = {
+      location: e.target.location.value,
+      name: e.target.name.value,
+      number: e.target.number.value
+    }
+  fetch(`${url}/stores`, {
+    method: "POST",
+    headers: {"content-type": "application/json"},
+    body: JSON.stringify(store)
+  })
+  .then(res => res.json())
+  .then(data=> addSelectOptionForStore(data))
+  .catch(err => alert("Something Went Wrong"))
+})
 
 
 /* fetches and renders all books */
